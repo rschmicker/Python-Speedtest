0d328ec4a41ff4bbe6b1250b31a21e77fc2f6545
Robert Schmicker <rschmicker@Roberts-MacBook-Pro.local>
2016-07-30 18:58:41 -0400

    fix time issue

--- client.py
+++ client.py
@@ -1,11 +1,31 @@
 #!/usr/bin/python           # This is client.py file
 
-import socket               # Import socket module
+import socket                   # Import socket module
+import time
+import os
 
-s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-port = 12345                # Reserve a port for your service.
+s = socket.socket()             # Create a socket object
+host = socket.gethostname()     # Get local machine name
+port = 20000                     # Reserve a port for your service.
 
 s.connect((host, port))
-print s.recv(1024)
-s.close                     # Close the socket when done
+
+with open('received_file', 'wb') as f:
+    print 'file opened'
+    t_start = time.time()
+    print(t_start)
+    while True:
+        data = s.recv(1024)
+        print(data)
+        if not data:
+            break
+        # write data to a file
+        f.write(data)
+t_end = time.time() - t_start
+print(t_end)
+t_total = (t_end)/60
+size_of_file = os.path.getsize("received_file")
+f.close()
+print("Download:\t" + str(size_of_file/t_total))
+s.close()
+print('connection closed')
--- server.py
+++ server.py
@@ -1,15 +1,37 @@
-#!/usr/bin/python           # This is server.py file
+#!/usr/bin/python		   # This is server.py file
 
-import socket               # Import socket module
+import socket                   # Import socket module
+import os
 
-s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-port = 12345                # Reserve a port for your service.
-s.bind((host, port))        # Bind to the port
+f = open('upload_test',"wb")
+f.seek(52428800)
+f.write("\0")
+f.close()
 
-s.listen(5)                 # Now wait for client connection.
+port = 20000                    # Reserve a port for your service.
+s = socket.socket()             # Create a socket object
+host = socket.gethostname()     # Get local machine name
+s.bind((host, port))            # Bind to the port
+s.listen(5)                     # Now wait for client connection.
+
+print 'Server listening....'
+
 while True:
-   c, addr = s.accept()     # Establish connection with client.
-   print 'Got connection from', addr
-   c.send('Thank you for connecting')
-   c.close()                # Close the connection
+    conn, addr = s.accept()     # Establish connection with client.
+    print 'Got connection from', addr
+    data = conn.recv(1024)
+    print('Server received', repr(data))
+
+    filename='upload_test'
+    f = open(filename,'rb')
+    l = f.read(1024)
+    while (l):
+       conn.send(l)
+       l = f.read(1024)
+    f.close()
+
+    print('Done sending')
+    conn.send('Thank you for connecting')
+    conn.close()
+
+os.remove("upload_test")
