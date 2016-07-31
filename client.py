c52323d222e577948c6c57bad2812bd4e9f83f0b
Robert Schmicker <rschmicker@Roberts-MacBook-Pro.local>
2016-07-30 20:59:43 -0400

    ver. 1.1

--- client.py
+++ client.py
@@ -1,40 +1,27 @@
-#!/usr/bin/python		   # This is client.py file
-import socket				   # Import socket module
+import socket
 import time
 import os
 
-s = socket.socket()			 # Create a socket object
-host = socket.gethostname()	 # Get local machine name
-port = 40000					# Reserve a port for your service.
+s = socket.socket()			 
+host = socket.gethostname()	 
+port = 10000
 
 s.connect((host, port))
 
 d_start = time.time()
-
 with open('received_file', 'wb') as f:
-	print 'file opened'
-	while True:
-		#print('receiving data...')
-		data = s.recv(1024)
-		#print('data=%s', (data))
-		if not data:
-			break
-		# write data to a file
-		f.write(data)
-d_end = time.time() - d_start
-"""
-l = f.read(1024)
-while (l):
-	conn.send(l)
-       #print('Sent ',repr(l))
-	l = f.read(1024)
-	f.close()
-"""
-size = os.path.getsize("received_file")
-print("Size:\t" + str(size))
-print("Time Download:\t" + str(d_end))
-speed = ((size/1024)/1024)/d_end
-print("Download:\t" + str(speed) + " MB/s")
+    while True:
+        data = s.recv(1024)
+        if not data:
+            break
+        f.write(data)
 f.close()
 s.close()
-print('connection closed')
+d_end = time.time() - d_start
+
+size = os.path.getsize("received_file")
+print("Size:\t\t" + str(size))
+print("Time Download:\t" + str(d_end))
+d_speed = ((size/1024)/1024)/d_end
+print("Download:\t" + str(d_speed) + " MB/s")
+os.remove("received_file")
--- server.py
+++ server.py
@@ -1,37 +1,28 @@
-#!/usr/bin/python		   # This is server.py file
-import socket                   # Import socket module
-import os
+import socket
 
-f = open('upload_test',"wb")
-f.seek(36700160)
+mb_size = 50
+f = open("upload","wb")
+f.seek(mb_size*1024*1024)
 f.write("\0")
 f.close()
 
-port = 40000                    # Reserve a port for your service.
-s = socket.socket()             # Create a socket object
-host = socket.gethostname()     # Get local machine name
-s.bind((host, port))            # Bind to the port
-s.listen(5)                     # Now wait for client connection.
+port = 10000
+s = socket.socket()
+host = socket.gethostname()
+s.bind((host, port))
+s.listen(5)
 
-print 'Server listening....'
+print "Server listening...."
 
 while True:
-    conn, addr = s.accept()     # Establish connection with client.
-    print 'Got connection from', addr
-    #data = conn.recv(1024)
-    #print('Server received', repr(data))
+	conn, addr = s.accept()
+	filename='upload'
+	f = open(filename,'rb')
+	l = f.read(1024)
+	while (l):
+	   conn.send(l)
+	   l = f.read(1024)
+	f.close()
 
-    filename='upload_test'
-    f = open(filename,'rb')
-    l = f.read(1024)
-    while (l):
-       conn.send(l)
-       #print('Sent ',repr(l))
-       l = f.read(1024)
-    f.close()
-
-    print('Done sending')
-    conn.send('Thank you for connecting')
-    conn.close()
-
-os.remove("upload_test")
+	print('Done sending')
+	conn.close()
